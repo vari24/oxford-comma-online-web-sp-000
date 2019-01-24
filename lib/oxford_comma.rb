7cfc587b0cc6e40c09022abcf463397f2e303937
Sara Varisara <varisara.n@gmail.com>
2019-01-24 02:53:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
-    array.join('')
+    array.join("")
   elsif array.length == 2
-    array.join('and')
-  else array.length == 3
-    array.join(', ') + array.join(', ') + array.join('and')
+    array.join(" and ")
   end
 end
